bb50ccee9e06f73d4574f224c77298af481fb076
spite400k <spite400k@gmail.com>
2023-02-28 09:37:44 +0900

    firebaseConfigを環境変数化

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,12 +1,12 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDKOetsHDiBpaqoi0S2Z00cuoRaE_dBXWg",
-    authDomain: "chatbot-demo-86c40.firebaseapp.com",
-    projectId: "chatbot-demo-86c40",
-    storageBucket: "chatbot-demo-86c40.appspot.com",
-    messagingSenderId: "281625461515",
-    appId: "1:281625461515:web:e0cbe0c22e8cc75416580e",
-    measurementId: "G-5D6EN26JYY"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 export default firebaseConfig;
